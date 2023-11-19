240cfeab18a45e26138dfca391c560deef81ece9
cybertronbot <okhuomonajayi54@gmail.com>
2023-11-19 21:16:26 +0100

    fix

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -4,9 +4,14 @@ import Navbar from "./Navbar";
 
 function Hero() {
   return (
-    <div className=" h-screen w-full bg-black">
-      <div className="  flex justify-center items-center">
+    <div className=" sm:h-[90vh] h-[100vh] w-full bg-black text-white ">
+      <div className="  flex w-full h-full justify-center flex-col items-center">
         <Navbar />
+        <h1 className="silka font-bold xl:text-[56px] text-[40px] text-center leading-[46px]">Unlock Endless Energy</h1>
+        <p className="text-white silka xl:w-[700px]  font-bold text-center mt-10">
+          Energy storage is the missing link in the sustainable energy system.
+          Our mission is to unlock endless energy.
+        </p>
       </div>
     </div>
   );
