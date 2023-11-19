f7ef2b736f449880cec7ab8b1ca6cff51f5badd0
cybertronbot <okhuomonajayi54@gmail.com>
2023-11-19 23:23:19 +0100

    numbers

--- app/components/Numbers.js
+++ app/components/Numbers.js
@@ -1,17 +1,30 @@
-import React from 'react'
-import pol from "@/public/pol.webp"
-import Image from 'next/image'
+import React from "react";
 
 function Numbers() {
   return (
     <div>
-      <div className='flex justify-center items-center px-6 '>
-       <div className='w-[100%] sm:h-full h-[40vh] rounded-3xl'>
-       <Image className='rounded-3xl w-full h-full sm:object-none object-cover' src={pol}/>
-       </div>
+        <h3 className="sm:text-[40px] text-[30px] sm:leading-[45px] leading-[40px] text-center mb-20 font-bold silka mt-40">
+              Polarium in Numbers
+            </h3>
+      <div className="flex xl:justify-between justify-center xl:flex-row flex-col xl:gap-0 gap-14 items-center xl:px-72">
+        <div className="flex justify-center items-center flex-col">
+          <h4 className="silka text-[50px]  font-bold text-center">2015</h4>
+          <hr className="w-[70px]  h-[1px] bg-[#cccccc] my-3" />
+          <h6 className="silka text-center">Founded</h6>
+        </div>
+        <div className="flex justify-center items-center flex-col">
+          <h4 className="silka text-[50px]  font-bold text-center">~550</h4>
+          <hr className="w-[70px]  h-[1px] bg-[#cccccc] my-3" />
+          <h6 className="silka text-center">People</h6>
+        </div>
+        <div className="flex justify-center items-center flex-col">
+          <h4 className="silka text-[50px]  font-bold text-center">440,000</h4>
+          <hr className="w-[70px]  h-[1px] bg-[#cccccc] my-3" />
+          <h6 className="silka text-center">Battery Installed</h6>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Numbers
+export default Numbers;
--- app/components/WhatWeDo.js
+++ app/components/WhatWeDo.js
@@ -1,9 +1,11 @@
 import React from "react";
+import pol from "@/public/pol.webp";
+import Image from "next/image";
 
 function WhatWeDo() {
   return (
     <div>
-      <div className="w-full flex flex-col justify-center items-center text-center pb-24 px-6 ">
+      <div className="w-full flex flex-col justify-center items-center text-center  px-6 ">
         <h3 className="sm:text-[40px] text-[30px] sm:leading-[45px] leading-[40px] font-bold silka">
           Safe, Smart and Sustainable <br /> Energy Storage
         </h3>
@@ -19,6 +21,12 @@ function WhatWeDo() {
         <button className=" mt-10 text-xs py-3.5 px-10 silka font-medium text-white bg-[#000] rounded-[50px]">
           What We DO
         </button>
+        <div className="w-[100%] sm:h-full h-[40vh] rounded-3xl pt-24">
+          <Image
+            className="rounded-3xl w-full h-full sm:object-none object-cover"
+            src={pol}
+          />
+        </div>
       </div>
     </div>
   );
