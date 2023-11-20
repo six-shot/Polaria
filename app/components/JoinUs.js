33bc8b6ff129be177dff58b35dbe19664dfa8c9c
cybertronbot <okhuomonajayi54@gmail.com>
2023-11-20 13:23:31 +0100

    fix

--- app/components/JoinUs.js
+++ app/components/JoinUs.js
@@ -4,8 +4,8 @@ import join from "@/public/foot.webp";
 function JoinUs() {
   return (
     <div className="max-w-[144opx] mx-auto my-28 ">
-      <div className="flex justify-center items-center">
-        <div className="h-[70vh] relative w-[80vw] rounded-3xl flex justify-center items-center   bg-black text-white  ">
+      <div className="flex xl:justify-center items-center 2xl:px-0 xl:px-10 px-6">
+        <div className="h-[70vh] relative xl:w-[80vw]  p-2 rounded-3xl flex justify-center items-center   bg-black text-white  ">
           <div className="absolute top-0 left-0 w-full h-full rounded-3xl overflow-hidden bg-black">
             <Image
               src={join}
@@ -19,7 +19,7 @@ function JoinUs() {
             <h1 className="silka font-bold xl:text-[48px] text-[40px] text-center leading-[46px] z-50">
               Join Our Journey
             </h1>
-            <p className="text-white silka xl:w-[500px] text-sm  font-medium text-center mt-10 z-50">
+            <p className="text-white silka xl:w-[500px] text-sm  font-medium text-center xl:mt-10 mt-5 z-50">
               We are on a rapid growth journey and are looking for great people
               to join our global and multi-cultural team. Do you want to empower
               a smart and sustainable world
